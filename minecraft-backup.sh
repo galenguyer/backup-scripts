7427e0e60072c31cae80457f4547eb5773b4cb85
Galen Guyer <galen@galenguyer.com>
2020-12-22 21:17:50 -0500

    add minecraft backup script

---
+++ minecraft-backup.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# fail in a sane manner
+set -eo pipefail
+
+# store date
+DATE="$(date +%Y-%m-%d)"
+DPATH="$(date +%Y/%m)/$DATE"
+
+# create temporary backup directory
+LOCALDIR="$(mktemp -d)"
+
+# remove the backup directory on exit
+function cleanup {
+  rm -rf "$LOCALDIR"
+}
+trap cleanup EXIT
+
+# set remote backup directory
+REMOTEDIR="Backups:minecraft/$DPATH"
+
+# create backup directory on dropbox
+rclone mkdir "$REMOTEDIR"
+
+# iterate through docker-compose volumes, compressing them into tarballs in $LOCALDIR
+for VOLUME in $(ls /root/minecraft/); do
+  docker run --rm -v "/root/minecraft/$VOLUME":/"$VOLUME":ro -v "$LOCALDIR":/target busybox tar czvf /target/"$DATE"_"$VOLUME".tgz /"$VOLUME"
+  rclone copy --verbose "$LOCALDIR"/"$DATE"_"$VOLUME".tgz "$REMOTEDIR"
+done
